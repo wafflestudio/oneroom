191218702ad14a435eda88d08e2103aca5040da6
Joo-shik Yoon <yyhpys@wafflestudio.com>
2012-03-31 18:29:56 +0900

    Initializing..

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oneroom::Application.config.secret_token = 'b635e7bc1112ca8dc694ce1ca8d93df2733f4487c9dcf6c4edd654ba1409154707e518861ab1f36bb2833c3e478e571d186f1edcbf9bcf92b5122413b65cf7e0'
